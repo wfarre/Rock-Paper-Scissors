9869123336b3f7572ac91e7e07c01089890eb913
wfarre <william.farre@gmail.com>
2024-02-10 21:55:06 +0800

    event listener

--- index.js
+++ index.js
@@ -205,14 +205,18 @@ const game = () => {
 
 console.log("Game will start! Be patient...");
 
-const startGamePromise = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(gameStart), 5000);
+window.addEventListener("focus", () => {
+  game();
 });
 
-startGamePromise
-  .then(() => game())
-  .catch(() =>
-    console.log(
-      "Too bad. You're too late. \n Please reload the page to start a new game. "
-    )
-  );
+// const startGamePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve(gameStart), 5000);
+// });
+
+// startGamePromise
+//   .then(() => game())
+//   .catch(() =>
+//     console.log(
+//       "Too bad. You're too late. \n Please reload the page to start a new game. "
+//     )
+//   );
